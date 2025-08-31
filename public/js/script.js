fcca8d529e5859238aaf32e3fb246a4b293782c6
Isabella Cardoso <isa.cardoso0447@gmail.com>
2025-08-31 13:14:55 -0300

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+  const busca = document.getElementById("busca");
+    const posts = document.querySelectorAll(".post");
+
+    function normalizarTexto(texto) {
+      return texto
+        .toLowerCase()
+        .normalize("NFD") 
+        .replace(/[\u0300-\u036f]/g, ""); 
+    }
+
+    busca.addEventListener("input", () => {
+      const termo = normalizarTexto(busca.value);
+
+      posts.forEach(post => {
+        const titulo = normalizarTexto(post.querySelector("h2").innerText);
+        const tags = normalizarTexto(post.querySelector(".tags").innerText);
+
+        if (titulo.includes(termo) || tags.includes(termo)) {
+          post.style.display = "block";
+        } else {
+          post.style.display = "none";
+        }
+      });
+    });
